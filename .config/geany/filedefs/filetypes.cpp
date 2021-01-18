fa90b2fc0fb25deb9418d29d43d9113671604139
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2021-01-18 20:07:43 +0000

    feat: Add new settings for Geany

--- .config/geany/filedefs/filetypes.cpp
+++ .config/geany/filedefs/filetypes.cpp
@@ -1,6 +1,6 @@
 [build-menu]
 FT_01_LB=_Build
-FT_01_CM=g++ -lpthread -Wall -o "%e" "%f"
+FT_01_CM=g++ -lpthread -Wall -g -o "%e" "%f"
 FT_01_WD=
 FT_02_LB=_Lint
 FT_02_CM=cppcheck --language=c++ --enable=warning,style --template=gcc "%f"
@@ -8,3 +8,6 @@ FT_02_WD=
 EX_00_LB=_Execute
 EX_00_CM="./%e"
 EX_00_WD=
+FT_00_LB=_Compile
+FT_00_CM=g++ -Wall -c -g "%f"
+FT_00_WD=
